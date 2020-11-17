442d26596c5e8f0ee6cdef37b1b013e98b770086
Manuel Mendoza <joseguns2@gmail.com>
2020-11-16 22:49:39 -0500

    Terminado :loop:
    :diamonds: :diamonds: :diamonds: :diamonds:

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,22 @@ var mediaqueryList = window.matchMedia("(max-width: 996px)");
         document.body.style='display:none'
         alert('Página Web no accesible para Tablet o Móbiles')
     }
+
+function change(){
+    const login = document.getElementById('login');
+    const register = document.getElementById('register');
+
+    if (login.style='display:flex'){
+        login.style='display:none'
+        register.style='display:flex'
+    }
+}
+function change2(){
+    const login = document.getElementById('login');
+    const register = document.getElementById('register');
+
+    if(login.style='display:none'){
+        login.style='display:flex'
+        register.style='display:none'
+    }
+}
