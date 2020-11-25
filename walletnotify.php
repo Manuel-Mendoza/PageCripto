e8366e6307f04e2a4512f298ccbc2cdebdea29d7
Carmelo Campos <31357533+CarmeloCampos@users.noreply.github.com>
2020-11-24 23:46:25 -0500

    update

--- walletnotify.php
+++ walletnotify.php
@@ -12,21 +12,23 @@ if (
 
     $rpc = RpcLoad();
     $info_txid = $rpc->gettransaction($txid);
-    if (isset($info_txid["details"]) && !empty($info_txid["details"])) {
-        $info_txid_detalles = $info_txid["details"];
+    if ($info_txid["confirmations"] == 0) {
+        if (isset($info_txid["details"]) && !empty($info_txid["details"])) {
+            $info_txid_detalles = $info_txid["details"];
 
-        foreach ($info_txid_detalles as $value_txid) {
-            if ($value_txid["category"] == "receive") {
-                $wallet = $value_txid["address"];
-                $buscar_id = mysqli_query($db, "SELECT * FROM `wallets` WHERE `wallet` = '$wallet'");
-                if (mysqli_num_rows($buscar_id) > 0) {
-                    $dato_wallet = mysqli_fetch_array($buscar_id);
-                    $user_id = $dato_wallet["user_id"];
-                    $all_data = getAllDataUser($user_id);
+            foreach ($info_txid_detalles as $value_txid) {
+                if ($value_txid["category"] == "receive") {
+                    $wallet = $value_txid["address"];
+                    $buscar_id = mysqli_query($db, "SELECT * FROM `wallets` WHERE `wallet` = '$wallet'");
+                    if (mysqli_num_rows($buscar_id) > 0) {
+                        $dato_wallet = mysqli_fetch_array($buscar_id);
+                        $user_id = $dato_wallet["user_id"];
+                        $all_data = getAllDataUser($user_id);
 
-                    $monto_Recibido = $all_data["balance"] + $value_txid["amount"];
-                    ActualizarBalance($user_id, $monto_Recibido);
-                    exit;
+                        $monto_Recibido = $all_data["balance"] + $value_txid["amount"];
+                        ActualizarBalance($user_id, $monto_Recibido);
+                        exit;
+                    }
                 }
             }
         }
